21017d1826fbae929f9e7c1e8301925697646175
TK <tgrl@protonmail.com>
2023-05-07 19:30:22 +0300

    Implemented updating stock

--- Assets/Scripts/Shops/Shop.cs
+++ Assets/Scripts/Shops/Shop.cs
@@ -23,8 +23,17 @@ namespace RPG.Shops
         }
         [SerializeField] private StockItemConfig[] stockConfig;
         private Dictionary<InventoryItem, int> transaction = new Dictionary<InventoryItem, int>();
+        private Dictionary<InventoryItem, int> stock = new Dictionary<InventoryItem, int>();
 
         public event Action onChange;
+
+        private void Awake()
+        {
+            foreach (StockItemConfig stockItem in stockConfig)
+            {
+                stock[stockItem.item] = stockItem.initialStock;
+            }
+        }
         public void SetShopper(Shopper shopper)
         {
             currentShopper = shopper;
@@ -45,8 +54,9 @@ namespace RPG.Shops
 
                 int quantityInTransaction = 0;
                 transaction.TryGetValue(config.item, out quantityInTransaction);
+                int currentStock = stock[config.item];
 
-                yield return new ShopItem(config.item, config.initialStock, price, quantityInTransaction);
+                yield return new ShopItem(config.item, currentStock, price, quantityInTransaction);
             }
         }
         public IEnumerable<ShopItem> GetFilteredItems()
@@ -60,7 +70,14 @@ namespace RPG.Shops
                 transaction[item] = 0;
             }
 
-            transaction[item] += quantity;
+            if (transaction[item] + quantity > stock[item])
+            {
+                transaction[item] = stock[item];
+            }
+            else
+            {
+                transaction[item] += quantity;
+            }
 
             if (transaction[item] <= 0)
             {
@@ -99,10 +116,12 @@ namespace RPG.Shops
                     if (isSuccessful)
                     {
                         AddToTransaction(item, -1);
+                        stock[item]--;
                         shopperPurse.UpdateBalance(-price);
                     }
                 }
             }
+            onChange?.Invoke();
         }
 
         public CursorType GetCursorType()
