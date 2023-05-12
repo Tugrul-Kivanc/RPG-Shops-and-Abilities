6711104f3bd1e453472706d9318947d781dd78e3
TK <tgrl@protonmail.com>
2023-05-12 18:40:46 +0300

    Added targeting API

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -6,7 +6,11 @@ namespace RPG.Abilities
     [CreateAssetMenu(fileName = "Default Ability", menuName = "Abilities/Ability", order = 0)]
     public class Ability : ActionItem
     {
+        [SerializeField] private TargetingStrategy targetingStrategy;
 
-
+        public override void Use(GameObject user)
+        {
+            targetingStrategy.StartTargeting();
+        }
     }
 }
---
+++ Assets/Scripts/Abilities/Targeting/DemoTargeting.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Abilities.Targeting
+{
+    [CreateAssetMenu(fileName = "Demo Targeting", menuName = "Abilities/Targeting/Demo", order = 0)]
+    public class DemoTargeting : TargetingStrategy
+    {
+        public override void StartTargeting()
+        {
+            Debug.Log("Demo Targeting Started");
+        }
+    }
+}
