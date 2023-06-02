7b7104ace94b83db45027631ff949653fbf51d45
TK <tgrl@protonmail.com>
2023-06-02 14:59:02 +0300

    Added Cooldown Indicators in UI & Refactoring

--- Assets/Scripts/Abilities/CooldownStore.cs
+++ Assets/Scripts/Abilities/CooldownStore.cs
@@ -1,16 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using GameDevTV.Inventories;
 using UnityEngine;
 
 namespace RPG.Abilities
 {
     public class CooldownStore : MonoBehaviour
     {
-        Dictionary<Ability, float> abilityCooldowns = new Dictionary<Ability, float>();
+        Dictionary<InventoryItem, float> abilityCooldowns = new Dictionary<InventoryItem, float>();
+        Dictionary<InventoryItem, float> initialCooldowns = new Dictionary<InventoryItem, float>();
 
         private void Update()
         {
-            var abilities = new List<Ability>(abilityCooldowns.Keys);
+            var abilities = new List<InventoryItem>(abilityCooldowns.Keys);
             foreach (var ability in abilities)
             {
                 abilityCooldowns[ability] -= Time.deltaTime;
@@ -18,21 +21,34 @@ namespace RPG.Abilities
                 if (abilityCooldowns[ability] <= 0)
                 {
                     abilityCooldowns.Remove(ability);
+                    initialCooldowns.Remove(ability);
                 }
             }
         }
 
-        public void StartCooldown(Ability ability, float cooldownTime)
+        public void StartCooldown(InventoryItem ability, float cooldownTime)
         {
             abilityCooldowns[ability] = cooldownTime;
+            initialCooldowns[ability] = cooldownTime;
         }
 
-        public float GetTimeRemaining(Ability ability)
+        public float GetTimeRemaining(InventoryItem ability)
         {
             if (!abilityCooldowns.ContainsKey(ability))
                 return 0;
 
             return abilityCooldowns[ability];
+        }
+
+        internal float GetFractionRemaining(InventoryItem ability)
+        {
+            if (ability == null)
+                return 0;
+
+            if (!abilityCooldowns.ContainsKey(ability))
+                return 0;
+
+            return abilityCooldowns[ability] / initialCooldowns[ability];
         }
     }
 }
