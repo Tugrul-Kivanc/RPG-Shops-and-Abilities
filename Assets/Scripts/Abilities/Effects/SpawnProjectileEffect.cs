267b8691dbe226e08ced3693e4e8c66b3d38d92f
TK <tgrl@protonmail.com>
2023-06-02 18:33:07 +0300

    Implemented Directional Targeting & Added Fireball Ability

--- Assets/Scripts/Abilities/Effects/SpawnProjectileEffect.cs
+++ Assets/Scripts/Abilities/Effects/SpawnProjectileEffect.cs
@@ -13,12 +13,30 @@ namespace RPG.Abilities.Effects
         [SerializeField] private Projectile projectileToSpawn;
         [SerializeField] private float damageAmount;
         [SerializeField] private bool isRightHand = true;
+        [SerializeField] private bool useTargetPoint = true;
 
         public override void StartEffect(AbilityData abilityData, Action finished)
         {
             Fighter fighter = abilityData.User.GetComponent<Fighter>();
             Vector3 spawnPosition = fighter.GetHandTransform(isRightHand).position;
 
+            if (useTargetPoint)
+                SpawnProjectileForTargetPoint(abilityData, spawnPosition);
+            else
+                SpawnProjectilesForTargets(abilityData, spawnPosition);
+
+            finished();
+        }
+
+        private void SpawnProjectileForTargetPoint(AbilityData abilityData, Vector3 spawnPosition)
+        {
+            Projectile projectile = Instantiate(projectileToSpawn);
+            projectile.transform.position = spawnPosition;
+            projectile.SetTarget(abilityData.User, abilityData.TargetedPoint, damageAmount);
+        }
+
+        private void SpawnProjectilesForTargets(AbilityData abilityData, Vector3 spawnPosition)
+        {
             foreach (var target in abilityData.Targets)
             {
                 Health health = target.GetComponent<Health>();
@@ -29,8 +47,6 @@ namespace RPG.Abilities.Effects
                 projectile.transform.position = spawnPosition;
                 projectile.SetTarget(health, abilityData.User, damageAmount);
             }
-
-            finished();
         }
     }
 }
