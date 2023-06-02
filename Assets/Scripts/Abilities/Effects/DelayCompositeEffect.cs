fb4b24aeef43da721050809780eb23a248504227
TK <tgrl@protonmail.com>
2023-06-02 16:37:06 +0300

    Implemented Ability Delay

---
+++ Assets/Scripts/Abilities/Effects/DelayCompositeEffect.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Abilities.Effects
+{
+    [CreateAssetMenu(fileName = "Delay Composite Effect", menuName = "Abilities/Effects/Delay Composite", order = 0)]
+    public class DelayCompositeEffect : EffectStrategy
+    {
+        [SerializeField] private float delay = 0;
+        [SerializeField] private List<EffectStrategy> delayedEffects;
+
+        public override void StartEffect(AbilityData abilityData, Action finished)
+        {
+            abilityData.StartCoroutine(DelayedEffect(abilityData, finished));
+        }
+
+        private IEnumerator DelayedEffect(AbilityData abilityData, Action finished)
+        {
+            yield return new WaitForSeconds(delay);
+
+            foreach (var effect in delayedEffects)
+            {
+                effect.StartEffect(abilityData, finished);
+            }
+        }
+    }
+}
